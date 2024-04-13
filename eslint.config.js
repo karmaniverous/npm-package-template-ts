0bb0e540e8c718863f95e55cf119e032d2ba1aaf
Jason Williscroft <me@karmanivero.us>
2024-04-13 16:48:48 +0800

    installed prettier & configured eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,31 @@
-// @ts-check
-
 import eslint from '@eslint/js';
+import prettierPlugin from 'eslint-config-prettier';
+import mochaPlugin from 'eslint-plugin-mocha';
+import simpleImportSortPlugin from 'eslint-plugin-simple-import-sort';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.strictTypeChecked,
   ...tseslint.configs.stylisticTypeChecked,
-
+  mochaPlugin.configs.flat.recommended,
   {
+    extends: [prettierPlugin],
+
     languageOptions: {
       parserOptions: {
         project: true,
         tsconfigRootDir: import.meta.dirname,
       },
     },
-  }
+    plugins: { 'simple-import-sort': simpleImportSortPlugin },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'error',
+      'mocha/no-skipped-tests': 'off',
+      'mocha/no-top-level-hooks': 'off',
+      'no-unused-vars': 'off',
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+    },
+  },
 );
