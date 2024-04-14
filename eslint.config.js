2f814f5e3a42e36a8b8770f7ceac38f8b6b3336a
Jason Williscroft <me@karmanivero.us>
2024-04-14 16:50:58 +0800

    disabled lint errors

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,12 @@
 import eslint from '@eslint/js';
 import prettierPlugin from 'eslint-config-prettier';
 import mochaPlugin from 'eslint-plugin-mocha';
+// @ts-expect-error: could not find a type declaration file
 import simpleImportSortPlugin from 'eslint-plugin-simple-import-sort';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
   eslint.configs.recommended,
   ...tseslint.configs.strictTypeChecked,
   ...tseslint.configs.stylisticTypeChecked,
@@ -18,6 +20,7 @@ export default tseslint.config(
         tsconfigRootDir: import.meta.dirname,
       },
     },
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     plugins: { 'simple-import-sort': simpleImportSortPlugin },
     rules: {
       '@typescript-eslint/no-unused-vars': 'error',
