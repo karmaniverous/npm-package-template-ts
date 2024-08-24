10d7df25da12e14918382550f3fbbae891581ccb
Jason Williscroft <me@karmanivero.us>
2024-08-24 15:37:44 +0800

    updated docs

--- docs/media/foo.test.ts
+++
@@ -1,9 +0,0 @@
-import { expect } from 'chai';
-
-import { foo } from './';
-
-describe('foo', function () {
-  it('should prefix bar', function () {
-    expect(foo('bar')).to.equal('foo bar');
-  });
-});
