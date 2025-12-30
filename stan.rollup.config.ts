dc33cf6a1b978ead9a8fb94f48c5dc3a47ee141f
Jason Williscroft <me@karmanivero.us>
2025-12-30 16:12:24 +0800

    removed redundant file

--- stan.rollup.config.ts
+++
@@ -1,7 +0,0 @@
-/** See <stanPath>/system/stan.project.md for global requirements. */
-
-import { buildLibrary, buildTypes } from './rollup.config';
-
-const outputPath = '.stan/dist';
-
-export default [buildLibrary(outputPath), buildTypes(outputPath)];
